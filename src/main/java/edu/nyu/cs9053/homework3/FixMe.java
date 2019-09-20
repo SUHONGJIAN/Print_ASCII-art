1c66a26f5582897492d3cb125a26750bdd1b2ba6
hongjian su <hongjiansu@hongjiandeMBP.fios-router.home>
2019-09-19 23:28:16 -0400

    For the first task: (compile command) javac -sourcepath src/main/java/ src/main/java/edu/nyu/cs9053/homework3/FixMe.java

--- src/main/java/edu/nyu/cs9053/homework3/FixMe.java
+++ src/main/java/edu/nyu/cs9053/homework3/FixMe.java
@@ -1,21 +1,25 @@
 package edu.nyu.cs9053.homework3;
+import edu.nyu.cs9053.homework3.metadata.*;   //import dependent class.
 
 /**
  * User: blangel
  */
 public class FixMe {
 
+    private static final String DEFAULT_METADATA = "";   //add DEFAULT_METADATA.
+
     private final String name;
 
-    public final String metadata;
+    private final String metadata;   //change public to private, because all instance variables should be private for good encapsulation and immutability.
 
     public FixMe(String name) {
-        this.name = name;
+        //this.name = name;   //delete this code, because there will be a error if metadata isn't initialized.
+	this(name, DEFAULT_METADATA);   // calls FixMe(String, String).
     }
 
     public FixMe(String name, String metadata) {
         this.name = name;
-        metadata = new Processor(true).process(metadata);
+        this.metadata = new Processor(true).processMetadata(metadata);   //change metadata to this.metadata, because metadata refers to the parameter in this function, and this.metadata refers to the variable in instance field. change the function process to processMetadata according to Processor.java.
     }
 
     public FixMe changeName(String name) {
@@ -23,15 +27,15 @@ public class FixMe {
     }
 
     public FixMe changeName(String firstName, String lastName) {
-        return new FixMe(changeName(firstName, lastName), metadata);
+        return new FixMe(combineName(firstName, lastName), metadata);   //change changeName() to combineName().
     }
 
-    public String changeName(String firstName, String lastName) {
+    public String combineName(String firstName, String lastName) {   //change the function name changeName to combineName for avoiding same function-name conflict, and also make the name clearer. 
         return String.format("%s %s", firstName, lastName);
     }
 
     public String getName() {
-        return name;
+        return this.name;   //change name to this.name to make it more clearly.
     }
 
 }
--- src/main/java/edu/nyu/cs9053/homework3/metadata/Processor.java
+++ src/main/java/edu/nyu/cs9053/homework3/metadata/Processor.java
@@ -3,16 +3,16 @@ package edu.nyu.cs9053.homework3.metadata;
 /**
  * User: blangel
  */
-class Processor {
+public class Processor {   //change <default> to public, because this class will be used in another package, edu.nyu.cs9053.homework3 
 
     private final boolean strict;
 
-    Processor(boolean strict) {
+    public Processor(boolean strict) {   //change <default> to public, because this method will be used in another package.
         this.strict = strict;
     }
 
     public String processMetadata(String metadata) {
-        return strict : String.format("\"%d\"", metadata) ? metadata;
+        return strict ? String.format("\"%d\"", metadata) : metadata;   //change : to ?, ? to :,because the syntax is b?x:y
     }
 
 }
